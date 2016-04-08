c73d9a5df72dab876b3e57078428786cf41ce933
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-04-08 11:36:54 +0200

    boletin25b

---
+++ src/boletin25b/Boletin25b.java
@@ -0,0 +1,14 @@
+package boletin25b;
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Boletin25b extends JFrame{
+            
+    public static void main(String[] args) {
+        FBoletin25b obx= new FBoletin25b();
+        obx.setVisible(true);
+        
+    }
+    
+}
